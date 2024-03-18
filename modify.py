96965cd6a269ac23c9d81ba39cef362c466b4b41
sanketkittad <sanketkittad02@gmail.com>
2024-03-18 16:00:04 +0530

    updated

--- modify.py
+++ modify.py
@@ -2,8 +2,8 @@ import os
 import shutil
 
 # Specify the directories to compare
-dir1 = "D:\BE-Project-23-24\model\Vivo"  # Replace with the actual path to directory A
-dir2 = "D:\BE-Project-23-24\model\Xiaomi"  # Replace with the actual path to directory B
+dir1 = "D:\BE-Project-23-24\model\Samsung"  # Replace with the actual path to directory A
+dir2 = "D:\BE-Project-23-24\model\Oppo"  # Replace with the actual path to directory B
 
 dir1_list=os.listdir(dir1)
 dir2_list=os.listdir(dir2)
@@ -23,7 +23,7 @@ for i in dir2_list:
 for i in range(len(dir1_list)):
     file1_path = os.path.join(dir1, dir1_list[i])
     file2_path = os.path.join(dir2, dir2_list[i])
-    new_file2_path = os.path.join(dir2, "x"+dir1_list[i][1:])  # Construct new path with filename from dir1
+    new_file2_path = os.path.join(dir2, "o"+dir1_list[i][1:])  # Construct new path with filename from dir1
     if os.path.exists(new_file2_path):
         print("already_there")
         continue
